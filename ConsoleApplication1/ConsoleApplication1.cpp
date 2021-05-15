992af266994958fed5c119c2212aa1c680fc37ea
Hy5121234QQQQ <79709601+Hy5121234QQQQ@users.noreply.github.com>
2021-05-15 12:37:13 +0800

    在不同的IDE里有不同的效果
    VS一种结果，DEV里又是另一种结果

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 7;
+	int j=(i++) + (i++) + (i++);
+	cout << i << "    " << j << endl;
+	j = (++i) + (++i) + (++i);
+	cout << i << "    " << j << endl;
+	return 0;
+}
+
+
